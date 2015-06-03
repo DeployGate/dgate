e399848a99f61ff2240caf1863567ee8b0c220c1
henteko <henteko07@gmail.com>
2015-06-02 17:34:04 -0700

    branchをmasterのみにした

--- release.sh
+++ release.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-gox -output "bin/{{.OS}}_{{.Arch}}_{{.Dir}}"
-VERSION=`grep Version version.go | cut -c 24- | sed -e 's/"//g'`
-ghr -u DeployGate -r dgate --token $GITHUB_TOKEN --replace $VERSION bin/
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+if test $BRANCH = "master" ; then
+  gox -output "bin/{{.OS}}_{{.Arch}}_{{.Dir}}"
+  VERSION=`grep Version version.go | cut -c 24- | sed -e 's/"//g'`
+  ghr -u DeployGate -r dgate --token $GITHUB_TOKEN --replace $VERSION bin/
+fi
